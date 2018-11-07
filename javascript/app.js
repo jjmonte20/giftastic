78331a8d4e08e482075f97b11ab53b4667c5114f
jjmonte20 <jjmonte20@gmail.com>
2018-11-07 08:12:21 -0600

    made still and animate states that change on click

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,7 @@
 // Need variable for api key
 var superheroes = ["Black Panther", "Captain America", "Doctor Strange"];
 
-var gurl = "http://api.giphy.com/v1/gifs/search?q="
+var gurl = "http://api.giphy.com/v1/gifs/search?q=";
 // query url and search term
 function renderButtons() {
 
@@ -23,6 +23,7 @@ function renderButtons() {
         $(".extendedUniverse").append(a);
     }
 }
+
 $(".extendedUniverse").on("click", '.superhero', function(){
     var name = $(this).attr("data-name");
     name = name.replace(/\s/g,"+")
@@ -51,13 +52,13 @@ $(".extendedUniverse").on("click", '.superhero', function(){
                 var heroImg = $("<img>");
 
                 // give the image tag a source pulled from the result item
-                heroImg.attr("src", results[i].images.fixed_height.url);
+                heroImg.attr("src", results[i].images.fixed_height_still.url);
 
                 // need to add two more attributes and have the gif start as data state still
-                // heroImg.attr("data-still", results[i].images.fixed_height_still.url);
-                // heroImg.attr("data-animate", results[i].images.fixed_height.url);
-                // heroImg.attr("data-state", "still");
-                // heroImg.attr("class", "gif")
+                heroImg.attr("data-still", results[i].images.fixed_height_still.url);
+                heroImg.attr("data-animate", results[i].images.fixed_height.url);
+                heroImg.attr("data-state", "still");
+                heroImg.attr("class", "gif")
 
                 
                 //now to add the gifs
@@ -68,25 +69,26 @@ $(".extendedUniverse").on("click", '.superhero', function(){
             }
             console.log(response);
         })
-})
+});
 
 // introduce a new variable called state that is based on an on click function
-/*
-// $(".gif").on("click", function(){
+
+$(document.body).on("click", ".gif", function(){
+    console.log("I've been clicked");
     //introduce a variable that identifies data state
     var state = $(this).attr("data-state");
     console.log(state);
 
     //check if state is still, if so, update to animate, and reverse as well
-    //if (state == "still") {
+    if (state == "still") {
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
     } else {
         $(this).attr("src", $(this).attr("data-still"));
         $(this).attr("data-state", "still");
     }
-})
-*/
+});
+
 $("#findHero").on("click", function(event){
     // can't have superhero clones
     event.preventDefault();
